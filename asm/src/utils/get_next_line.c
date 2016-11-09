8ef71653297e1c440ebc5cf867d99e1ab6145c14
Guillaume MARDON <guillaume.mardon@epitech.eu>
2016-11-09 13:30:13 +0100

    begin asm

---
+++ asm/src/utils/get_next_line.c
@@ -0,0 +1,13 @@
+/*
+** get_next_line.c for corewar in /media/gmardon/1aa9b3b8-3e24-4ea9-9b0d-d57254b2d1b9/guillaume.mardon/delivery/CPE_2016_corewar/asm/src/utils/
+**
+** Made by Guillaume MARDON
+** Login   <guillaume.mardon@epitech.eu@epitech.eu>
+**
+** Started on  Wed Nov  9 13:26:44 2016 Guillaume MARDON
+** Last update Wed Nov  9 13:26:49 2016 Guillaume MARDON
+*/
+char *get_next_line(const int fd)
+{
+
+}
