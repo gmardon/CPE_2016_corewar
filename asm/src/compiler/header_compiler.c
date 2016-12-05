5de687cde52c0efb112394fd436606a1dd1a83b1
Guillaume MARDON <guillaume.mardon@epitech.eu>
2016-12-05 17:50:00 +0100

    fixed

--- asm/src/compiler/header_compiler.c
+++ asm/src/compiler/header_compiler.c
@@ -5,7 +5,7 @@
 ** Login   <guillaume.mardon@epitech.eu@epitech.eu>
 **
 ** Started on  Mon Dec  5 16:37:09 2016 Guillaume MARDON
-** Last update Mon Dec  5 17:16:21 2016 Guillaume MARDON
+** Last update Mon Dec  5 17:48:06 2016 Guillaume MARDON
 */
 #include "../../include/asm.h"
 
@@ -13,8 +13,8 @@ void	add_magic(char* buffer, int *index)
 {
   int magic = COREWAR_EXEC_MAGIC;
   char m1 = (magic >> 16) & 0xff;
-  char m2 = (COREWAR_EXEC_MAGIC >> 8) & 0xff;
-  char m3 = COREWAR_EXEC_MAGIC & 0xff;
+  char m2 = (magic >> 8) & 0xff;
+  char m3 = magic & 0xff;
   buffer[*index] = 0x00;
   *index += 1;
   buffer[*index] = m1;
@@ -28,6 +28,11 @@ void	add_magic(char* buffer, int *index)
 void	compile_header(char* buffer, int *index, char* name, char* comment)
 {
   add_magic(buffer, index);
-  write_to_buffer(buffer, index, right_padding("name", 4, PROG_NAME_LENGTH),
-		  PROG_NAME_LENGTH);
+  write_to_buffer(buffer, index,
+		  right_padding(name, my_strlen(name),
+				PROG_NAME_LENGTH), PROG_NAME_LENGTH);
+
+	write_to_buffer(buffer, index,
+    	right_padding(comment, my_strlen(comment),
+		      COMMENT_LENGTH), COMMENT_LENGTH);
 }
